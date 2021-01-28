22deaff90a325a3024144157a1f1f1cc9eb651f6
jpjonela <77683261+jpjonela@users.noreply.github.com>
2021-01-28 11:39:38 -0500

    Lab 1 jpjonela
    My First Lab for 220

---
+++ Labs/Lab1/js/app.js
@@ -0,0 +1,29 @@
+function setup() {
+    //Code for Creating Canvas
+    createCanvas(1200, 800)
+
+
+    
+    //Color Settings for Car
+    fill(0,153,153)
+    stroke(0,0,0)
+
+    //Create a Rectangle
+    rect(150,100,300,100)
+
+
+
+
+    //Color settings for Wheels & Front of Car
+    fill(255,0,0)
+
+    //Create a Circle
+    circle(220,250,50)
+
+    //Create Circle 2
+    circle(400,250,50)
+
+    //Create Rectangle front of Car
+    rect(100,150,50,50)
+
+}
