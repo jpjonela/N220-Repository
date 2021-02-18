d891014dc7d17c3a6b4711e4de8725092b20a698
jpjonela <77683261+jpjonela@users.noreply.github.com>
2021-02-18 12:03:45 -0500

    Lab4 JS

---
+++ Labs/Lab4/js/app.js
@@ -0,0 +1,14 @@
+function setup() {
+    //Code for Neapolitan
+    createCanvas(900, 900)
+
+    fillRect(0,0,300,300)
+    ctx.fillStyle = "#B14435"
+
+    fillRect(300,0,300,300)
+    ctx.fillStyle = "#BFFFFFF"
+
+    fillRect(600,0,300,300)
+    ctx.fillStyle = "#FF009B"
+}
+
